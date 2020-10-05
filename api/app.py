da560976440d4683a02abad5adc34627ed890da1
Fabiano Soder <fabiano.soder@cwi.com.br>
2020-10-05 16:06:57 -0300

    feat(api-integration): added search user endpoint

--- api/app.py
+++ api/app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask import request
+from instagram_web_api import Client, ClientCompatPatch, ClientError, ClientLoginError
+from requests import get
+
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/hello")
+@app.route("/search-user")
 def index():
-    return 'Olá Mundo!'
+    user = request.args.get('user')
+    user_search_url = 'https://www.instagram.com/web/search/topsearch/?query='
+    response = get(user_search_url + user).json()
+    
+    data = {
+        'userName': response['users'][0]['user']['username'],
+        'userId': response['users'][0]['user']['pk'],
+        'profileImage': response['users'][0]['user']['profile_pic_url']
+    }
+
+    return data
+
 
 if __name__ == "__main__":
     app.debug=True
