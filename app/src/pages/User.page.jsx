ab3f68ef4984f22a60c964ba60a92fe2b314a340
Fabiano Soder <fabiano.soder@cwi.com.br>
2020-10-05 09:39:53 -0300

    added initial page search css

--- app/src/pages/User.page.jsx
+++ app/src/pages/User.page.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 
+import './User.page.css';
+
 const User = props => {
+
+    const onsubmit = () => {
+        alert('clicado');
+    }
 
     const renderSearch = () => {
         return (
-            <div>
-                <p>teste</p>
+            <div className='searchContainer'>
+                <label className='userPageSearchLabel'>Nome de usuário</label>
+                <input type='text' name='userSearch' className='userPageSearchInput' placeholder='@fahsoder' />
+                <button className='userPageSearchSubmit' onClick={onsubmit}>Buscar</button>
             </div>
         )   
     }
 
 
     return (
-        <>
+        <div className='userPageContainer'>
             {renderSearch()}
-        </>
+        </div>
     )
 }
 
